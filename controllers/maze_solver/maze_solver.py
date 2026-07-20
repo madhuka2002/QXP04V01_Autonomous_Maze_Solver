152e3ae961a90eb9f0eaa4ed0bf44b3b0e6babaf
Madhuka Malshan <167554333+madhuka2002@users.noreply.github.com>
2026-07-20 07:55:56 +0530

    turn left, right, and turn around. Left hand rule

--- controllers/maze_solver/maze_solver.py
+++ controllers/maze_solver/maze_solver.py
@@ -1,7 +1,10 @@
 from controller import Robot
 
 TIME_STEP = 64
-MAX_SPEED = 6.28
+THRESHOLD = 80
+
+FORWARD_SPEED = 3.0
+TURN_SPEED = 2.0
 
 robot = Robot()
 
@@ -32,14 +35,94 @@ for name in sensor_names:
     sensor.enable(TIME_STEP)
     sensors.append(sensor)
 
-while robot.step(TIME_STEP) != -1:
-    values = [sensor.getValue() for sensor in sensors]
-    
-    print(
-        f"Front Right: {values[0]:.2f} | "
-        f"Front: {values[7]:.2f} | "
-        f"Left: {values[5]:.2f} | "
+
+def left_wall():
+    return (
+        sensors[5].getValue() > THRESHOLD
+        or sensors[6].getValue() > THRESHOLD
     )
-    
+
+
+def front_wall():
+    return (
+        sensors[0].getValue() > THRESHOLD
+        or sensors[7].getValue() > THRESHOLD
+    )
+
+
+def right_wall():
+    return (
+        sensors[1].getValue() > THRESHOLD
+        or sensors[2].getValue() > THRESHOLD
+    )
+
+
+def stop():
     left_motor.setVelocity(0.0)
     right_motor.setVelocity(0.0)
+
+
+def move_forward(duration = 10):
+    left_motor.setVelocity(FORWARD_SPEED)
+    right_motor.setVelocity(FORWARD_SPEED)
+
+    for _ in range(duration):
+        robot.step(TIME_STEP)
+
+    stop()
+
+
+def turn_left():
+    left_motor.setVelocity(-TURN_SPEED)
+    right_motor.setVelocity(TURN_SPEED)
+
+    for _ in range(12):
+        robot.step(TIME_STEP)
+
+    stop()
+
+
+def turn_right():
+    left_motor.setVelocity(TURN_SPEED)
+    right_motor.setVelocity(-TURN_SPEED)
+
+    for _ in range(12):
+        robot.step(TIME_STEP)
+
+    stop()
+
+
+def turn_around():
+    left_motor.setVelocity(TURN_SPEED)
+    right_motor.setVelocity(-TURN_SPEED)
+
+    for _ in range(24):
+        robot.step(TIME_STEP)
+
+    stop()
+
+
+while robot.step(TIME_STEP) != -1:
+
+    left = left_wall()
+    front = front_wall()
+    right = right_wall()
+
+    print(f"L:{left} F:{front} R:{right}")
+
+    if not front:
+        print("Forward")
+        move_forward()
+
+    else:
+        if not left:
+            print("Turn Left")
+            turn_left()
+
+        elif not right:
+            print("Turn Right")
+            turn_right()
+
+        else:
+            print("Turn Around")
+            turn_around()
